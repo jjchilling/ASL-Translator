fb611352c3d62f77f4d14992c66f199538261766
Hee Su Chung <jhschung88@gmail.com>
2024-04-23 21:24:10 -0400

    feat: main.sh to activate cs143 venv conda

--- code/main.sh
+++ code/main.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 echo "Running main.py..."
-VENV_PATH="/Users/julie_chung/miniforge3/envs/cs1430/lib/python3.9/venv"
-cd $(dirname $VENV_PATH)
+CONDA_PATH="/Users/my_name/miniforge3/bin/conda"
+source "${CONDA_PATH}/../etc/profile.d/conda.sh"
+conda activate cs1430
 if [ $? -eq 0 ]; then
-	echo "Changed directory to $(pwd)"
-	source "$VENV_PATH/bin/activate"
-	if [ $? -eq 0 ]; then
-        	echo "Virtual environment activated."
-	else
-		echo "Failed to activate virtual environment."
-		exit 1
-	fi
+	echo "Conda environment activated."
 else
-	echo "Failed to change directory."
+	echo "Failed to activate Conda environment."
 	exit 1
 fi
 python main.py --task 3
