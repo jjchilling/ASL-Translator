8c57c474c9f31bc5ebc08ad4cc70d058a4bb234d
Hee Su Chung <jhschung88@gmail.com>
2024-04-24 00:56:02 -0400

    fixed .sh

--- code/main.sh
+++ code/main.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 echo "Running main.py..."
-CONDA_PATH="/Users/julie_chung/miniforge3/bin/conda"
-source "${CONDA_PATH}/../etc/profile.d/conda.sh"
-conda env list
-conda activate cs1430
-if [ $? -eq 0 ]; then
-	echo "Conda environment activated."
-else
-	echo "Failed to activate Conda environment."
-	exit 1
-fi
 python main.py --task 3
